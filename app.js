a36625968c9502c757c967a69da11c57926c5de8
Pillsang Sung <schmidtiana95@gmail.com>
2022-04-22 14:17:43 +0900

    FEAT span tag

--- app.js
+++ app.js
@@ -2,30 +2,41 @@ const toDoForm = document.querySelector('.to_do_form');
 const toDoFormInput = document.querySelector('.to_do_form Input');
 const toDoListUl = document.querySelector('.to_do_list_ul');
 const TODOS_KEY = 'todos';
+const DONEDOS_KEY = 'donedos';
 
 let toDos = localStorage.getItem(TODOS_KEY) ? JSON.parse(localStorage.getItem(TODOS_KEY)) : [];
+let doneDos = localStorage.getItem(DONEDOS_KEY) ? JSON.parse(localStorage.getItem(DONEDOS_KEY)) : [];
 
 const saveToDo = (toDos) => {
   localStorage.setItem('todos', JSON.stringify(toDos));
 };
 const saveDoneDo = (doneDos) => {
   localStorage.setItem('doneDos', JSON.stringify(doneDos));
-}
+};
 const paintToDo = (newToDo) => {
   const li = document.createElement('li');
   const span = document.createElement('span');
-  const buttonDone = document.createElement('button');
-  const buttonDelete = document.createElement('button');
-  
-  buttonDone.innertext = 'V';
-  buttonDone.className = 'btn_done'
-  buttonDone.addEventListener('click', () => {
+  const buttonDone = document.createElement('span');
+  const buttonDelete = document.createElement('span');
 
+  buttonDelete.innerText = 'done';
+  buttonDelete.className = 'material-symbols-outlined';
+  buttonDelete.addEventListener('click', (event) => {
+    const li = event.target.parentElement;
+    li.remove();
+    doneDos.push(newToDo);
+    toDos = toDos.filter((newToDo) => {
+      if (newToDo.id !== parseInt(li.id)) {
+        return newToDo;
+      }
+    });
+    saveToDo(toDos);
+    saveDoneDo(doneDos);
   });
 
-  buttonDelete.innerText = 'X';
-  buttonDelete.className = 'btn_delete'
-  buttonDelete.addEventListener('click', (event) => {
+  buttonDone.innerText = 'close';
+  buttonDone.className = 'material-symbols-outlined';
+  buttonDone.addEventListener('click', () => {
     const li = event.target.parentElement;
     li.remove();
     toDos = toDos.filter((newToDo) => {
@@ -40,6 +51,7 @@ const paintToDo = (newToDo) => {
   li.appendChild(span);
   li.appendChild(buttonDone);
   li.appendChild(buttonDelete);
+
   span.innerText = newToDo.text;
   toDoListUl.appendChild(li);
 };
@@ -56,7 +68,7 @@ toDoForm.addEventListener('submit', (event) => {
 
   toDos.push(newToDoObj);
   saveToDo(toDos);
-  paintToDo(newToDoObj)
+  paintToDo(newToDoObj);
 });
 
 if (toDos !== null) {
