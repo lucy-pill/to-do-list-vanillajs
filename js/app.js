7556207e7dcc06b35bdb1fb2032c4f27338926ff
bibisung <schmidtiana95@gmail.com>
2022-05-01 20:40:43 +0900

    FEAT modal button add, explain text modify

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
+// Constants
 const toDoForm = document.querySelector('.to_do_form');
 const toDoFormInput = document.querySelector('.to_do_form Input');
 const toDoListUl = document.querySelector('.to_do_list_ul');
+const buttonElement = document.querySelector('body .btn_modal');
+const modalElement = document.querySelector('.modal');
+const modalListElement = document.querySelector('.modal .modal_list');
+const modalListUlElement = document.querySelector('.modal .modal_list ul');
+const modalClearElement = document.querySelector('.modal .modal_clear');
+const modalListUl = document.querySelector('.modal_list_ul');
 const TODOS_KEY = 'todos';
 const DONEDOS_KEY = 'doneDos';
 const MONTHS = [
@@ -18,9 +25,12 @@ const MONTHS = [
   'December',
 ];
 
+// Variables
+let trigger = false;
 let toDos = localStorage.getItem(TODOS_KEY) ? JSON.parse(localStorage.getItem(TODOS_KEY)) : [];
 let doneDos = localStorage.getItem(DONEDOS_KEY) ? JSON.parse(localStorage.getItem(DONEDOS_KEY)) : [];
 
+// Functions
 const saveToDo = (toDos) => {
   localStorage.setItem('todos', JSON.stringify(toDos));
 };
@@ -33,6 +43,33 @@ const paintToDo = (newToDo) => {
   const buttonDone = document.createElement('span');
   const buttonDelete = document.createElement('span');
   const date = document.createElement('span');
+
+  buttonDone.innerText = 'done';
+  buttonDone.className = 'material-symbols-outlined';
+  buttonDone.addEventListener('click', () => {
+    const li = event.target.parentElement;
+    const today = new Date();
+    const time = `Finished time: ${MONTHS[today.getMonth()]} ${today.getDate()}, ${today.getFullYear()} | ${String(
+      today.getHours()
+    ).padStart(2, '0')}:${String(today.getMinutes()).padStart(2, '0')}:${String(today.getSeconds()).padStart(2, '0')}`;
+    const doneDoObj = {
+      id: newToDo.id,
+      text: newToDo.text,
+      CompleteDate: time,
+    };
+    li.remove();
+
+    doneDos.push(doneDoObj);
+    toDos = toDos.filter((newToDo) => {
+      if (newToDo.id !== parseInt(li.id)) {
+        return newToDo;
+      }
+    });
+
+    paintModalList(doneDoObj);
+    saveToDo(toDos);
+    saveDoneDo(doneDos);
+  });
 
   buttonDelete.innerText = 'close';
   buttonDelete.className = 'material-symbols-outlined';
@@ -47,21 +84,6 @@ const paintToDo = (newToDo) => {
     saveToDo(toDos);
   });
 
-  buttonDone.innerText = 'done';
-  buttonDone.className = 'material-symbols-outlined';
-  buttonDone.addEventListener('click', () => {
-    const li = event.target.parentElement;
-    li.remove();
-    doneDos.push(newToDo);
-    toDos = toDos.filter((newToDo) => {
-      if (newToDo.id !== parseInt(li.id)) {
-        return newToDo;
-      }
-    });
-    saveToDo(toDos);
-    saveDoneDo(doneDos);
-  });
-
   date.innerText = newToDo.createDate;
   li.id = newToDo.id;
   li.appendChild(span);
@@ -72,7 +94,50 @@ const paintToDo = (newToDo) => {
   span.innerText = newToDo.text;
   toDoListUl.appendChild(li);
 };
+const paintModalList = (doneToDo) => {
+  const li = document.createElement('li');
+  const spanText = document.createElement('span');
+  const spanTime = document.createElement('span');
+  const buttonDelete = document.createElement('span');
 
+  spanText.innerText = doneToDo.text;
+  spanTime.innerText = doneToDo.CompleteDate;
+  buttonDelete.innerText = 'close';
+  buttonDelete.className = 'material-symbols-outlined';
+  buttonDelete.addEventListener('click', (event) => {
+    const li = event.target.parentElement;
+    li.remove();
+    doneDos = doneDos.filter((doneToDo) => {
+      if (doneToDo.id !== parseInt(li.id)) {
+        return doneToDo;
+      }
+    });
+    saveDoneDo(doneDos);
+  });
+  li.appendChild(spanText);
+  li.appendChild(spanTime);
+  li.appendChild(buttonDelete);
+  modalListUl.appendChild(li);
+};
+const paintModal = () => {
+  if (trigger === false) {
+    modalElement.style.display = 'flex';
+    trigger = true;
+  } else {
+    modalElement.style.display = 'none';
+    trigger = false;
+  }
+};
+const clearModalList = () => {
+  const listElement = document.querySelectorAll('.modal .modal_list li');
+  for (let items of listElement) {
+    modalListUlElement.removeChild(items);
+  }
+  doneDos = [];
+  saveDoneDo(doneDos);
+};
+
+// MAIN PAGE PAINT
 toDoForm.addEventListener('submit', (event) => {
   event.preventDefault();
 
@@ -96,5 +161,15 @@ toDoForm.addEventListener('submit', (event) => {
 if (toDos !== null) {
   for (let items of toDos) {
     paintToDo(items);
+  }
+}
+
+// MODAL PAGE PAINT
+buttonElement.addEventListener('click', paintModal);
+modalClearElement.addEventListener('click', clearModalList);
+
+if (doneDos !== null) {
+  for (let items of doneDos) {
+    paintModalList(items);
   }
 }
